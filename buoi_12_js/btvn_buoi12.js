c844e6022d7befcb6b5ff026cb395ac4ea12bec4
NguyenTuanAnh <nguyenanh1812zxc@gmail.com>
2022-07-16 16:20:29 +0700

    fix display search

--- buoi_12_js/btvn_buoi12.js
+++ buoi_12_js/btvn_buoi12.js
@@ -1,15 +1,17 @@
 const tableDiv = document.querySelector('table')
 const btnSearch = document.getElementById("btnSearch")
 let listUsers = []
+let listSearch = []
 
 async function getApi() {
     await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
         .then(users => {
             listUsers = users
-            console.log(listUsers)
-            render(listUsers)
-            search(listUsers)
         });
+
+    // console.log(listUsers)
+    render(listUsers)
+    search(listUsers)
 } getApi()
 
 function render(array) {
@@ -17,53 +19,69 @@ function render(array) {
         const userRow = document.createElement('tr')
         userRow.setAttribute('id', `user-${index}`)
         userRow.setAttribute('class', `usersOld`)
-        userRow.innerHTML = `
-            <tr>
+        userRow.innerHTML = `       
                 <td>${id}</td>
                 <td>${name}</td>
                 <td>${email}</td>
                 <td>${phone}</td>
                 <td>${company.name}</td>
                 <td>${website}</td>
-            </tr>
             `
         tableDiv.append(userRow)
     });
 }
 
-function removeOldUser() {
-    const e = document.getElementsByClassName('userOld')    
-    for(let i of e){
-        document.getElementById( `user-${i}`).remove()
+// function removeOldUser() {
+//     const e = document.getElementsByClassName('userOld')    
+//     for(let i in e){
+//     //    document.getElementById(`user-${i}`).remove()
+//         e[i].remove()
+//     }
+//     console.log(e)
+// }
+
+function hideOldUsers() {
+    for (let i in listUsers) {
+        document.getElementById(`user-${i}`).style.display = "none"
+        for (let j in listSearch) {
+            if (listUsers[i] == listSearch[j]) {
+                document.getElementById(`user-${i}`).style.display = " table-row"
+            }
+        }
+    }
+}
+
+function showUsers(a) {
+    for (let i in a) {
+        document.getElementById(`user-${i}`).style.display = "block"
     }
 }
 
 function search(listUsers) {
     btnSearch.addEventListener('click', function () {
-        removeOldUser()
         const selectTypeSearch = document.getElementById("select-type-search").value
         const searchText = document.getElementById("search-input").value.toLocaleLowerCase()
         if (selectTypeSearch == "name") {
-            const listUsersName = listUsers.filter(({ name }) => name.toLocaleLowerCase().includes(searchText))
-            render(listUsersName)
-            console.log(listUsersName)
+            // const listUsersName = listUsers.filter(({ name }) => name.toLocaleLowerCase().includes(searchText))
+            // console.log(listUsers)
+            // render(listUsersName)
+            listSearch = listUsers.filter(({ name }) => name.toLocaleLowerCase().includes(searchText))
+            console.log(listSearch)
         }
         else if (selectTypeSearch == "phone") {
-            const listUsersPhone = listUsers.filter(({ phone }) => phone.toLocaleLowerCase().includes(searchText))
-            render(listUsersPhone)
+            listSearch = listUsers.filter(({ phone }) => phone.toLocaleLowerCase().includes(searchText))
         }
         else if (selectTypeSearch == "email") {
-            const listUsersEmail = listUsers.filter(({ email }) => email.toLocaleLowerCase().includes(searchText))
-            render(listUsersEmail)
+            listSearch = listUsers.filter(({ email }) => email.toLocaleLowerCase().includes(searchText))
         }
         else if (selectTypeSearch == "company") {
-            const listUsersCompany = listUsers.filter(({ company }) => company.name.toLocaleLowerCase().includes(searchText))
-            render(listUsersCompany)
+            listSearch = listUsers.filter(({ company }) => company.name.toLocaleLowerCase().includes(searchText))
         }
         else {
             alert('Không tìm thấy user!!')
             render(listUsers)
         }
+        hideOldUsers()
     })
 }
 
